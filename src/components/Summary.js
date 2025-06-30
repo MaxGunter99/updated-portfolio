570264af712a9126b57a4ada2230fbf95a46dfbe
Max Gunter <maxgunter99@gmail.com>
2025-06-30 02:22:33 -0500

    adding basic breakpoints

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -16,7 +16,7 @@ export default function Summary() {
             <img className="avatar-image" src={avatar} />  
 
             <div className='summary-content'>
-                <h3>About Me</h3>
+                <h2>About Me</h2>
                 <p>{config["JOB_TITLE"]} with 3+ years of experience building apps in React and Django. Bootcamp-trained with industry experience. Passionate about performance, resilience, and systems that work. Outside of coding, I enjoy flying a custom-built drone and discovering new music.</p>
             </div>
             
